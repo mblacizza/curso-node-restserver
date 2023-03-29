12451cfacb2bfe5f33f390dcc779ecf6fcc4265c
miketknf <mblacizza@gmail.com>
2023-03-29 18:06:10 -0300

    Fin Seccion 11 -version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 const login = async (req, res = response) => {
@@ -9,7 +10,7 @@ const login = async (req, res = response) => {
     const { correo, password } = req.body;
     try {
 
-        //verificar si esxiste el correo
+        //verificar si existe el correo
         const usuario = await Usuario.findOne({ correo });
         if (!usuario) {
             return res.status(400).json({
@@ -49,6 +50,55 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async( req, res = response) =>{
+    const { id_token } = req.body;
+
+    try {
+
+        const {nombre, img, correo} = await googleVerify(id_token);
+       
+        //Primero voy a verificar si ya existe el correo en mi DB de Mongoose
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            //Tengo q crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                estado: true,
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save(); 
+        }//Si ya existe se puede actualiza la data pero por ahora lo dejamos asi
+
+        //SI el usuario en DB esta activo
+        if (!usuario.estado){
+            return res.status(401).json({
+                msg: 'Usuario eliminado'
+            });
+        }
+
+        //Generar JWT
+        const token = await generarJWT(usuario.id);
+       
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({            
+            msg: 'El token de google no se pudo verificar'
+        })
+    }
+
+        
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
